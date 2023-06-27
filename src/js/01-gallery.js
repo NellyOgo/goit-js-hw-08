38a1f6ca901bff66b6512a9b768ea0277ebe3a57
NellyOgo <nellyogorodnik@gmail.com>
2023-06-27 23:03:09 +0300

    Try3 Nell

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,18 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 
+const gallery = document.querySelector('.gallery');
+galleryItems.forEach(item => {
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  link.href = item.original;
+  const image = document.createElement('img');
+  image.src = item.preview;
+  image.alt = item.description;
+  link.appendChild(image);
+  listItem.appendChild(link);
+  gallery.appendChild(listItem);
+});
 // Change code below this line
-
+const lightbox = new SimpleLightbox('.gallery a');
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,12 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-const iframe = document.querySelector('#video-player');
-Player.toString(
-    'timeupdate', throttle(function (data) {
-        localStorage.setItem('videoplayer-current-time', data.seconds);
-    }, 1000)
-);
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle(function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000));
+
 const currentTime = localStorage.getItem('videoplayer-current-time');
 if (currentTime !== null) {
-    Player.setCurrentTime(currentTime);
+  player.setCurrentTime(currentTime);
 }
